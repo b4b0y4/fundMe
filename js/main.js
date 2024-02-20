33a50385b669a5826177b2cde35961f37217fdd3
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2024-02-20 15:53:26 +0900

    refactor

--- js/main.js
+++ js/main.js
@@ -37,4 +37,21 @@ function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark })
     currentThemeSetting = newTheme
 
   })
-  
+
+// burger menu
+  document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenuButton = document.querySelector('.burger-menu-button')
+    const panel = document.getElementById('panel')
+    const closeButton = document.querySelector('.close-button')
+    const overlay = document.getElementById('overlay')
+
+    burgerMenuButton.addEventListener('click', function () {
+        panel.classList.toggle('open')
+        overlay.style.display = "block"
+    })
+
+    closeButton.addEventListener('click', function () {
+        panel.classList.remove('open')
+        overlay.style.display = "none"
+    })
+})
