97b1e8570e76c30adbde2484e680c100c45bc425
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2024-04-13 17:29:03 +0900

    switch contract

--- js/constants.js
+++ js/constants.js
@@ -2,18 +2,39 @@ export const networkConfigs = {
   ethereum: {
     name: "Ethereum",
     rpcUrl: "https://eth.llamarpc.com",
-    chainId: 1
+    chainId: 1,
   },
   sepolia: {
     name: "Sepolia",
     rpcUrl: "https://rpc.sepolia.org",
-    chainId: 11155111
-  }
+    chainId: 11155111,
+  },
 }
 
-export const contractAddress = "0x4D05a5a955Df8ee20c8C767064C3199481c1eb66"
+export const contractAddress = "0x7497eD6cf9564e53b970683388922D39C64F65c1"
 
 export const abi = [
+  {
+    inputs: [{ internalType: "address", name: "priceFeed", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  { inputs: [], name: "FundMe__NotOwner", type: "error" },
+  { stateMutability: "payable", type: "fallback" },
+  {
+    inputs: [],
+    name: "MINIMUM_USD",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "cheaperWithdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
   {
     inputs: [],
     name: "fund",
@@ -22,79 +43,41 @@ export const abi = [
     type: "function",
   },
   {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    inputs: [],
-    name: "FundMe__NotOwner",
-    type: "error",
-  },
-  {
-    stateMutability: "payable",
-    type: "fallback",
-  },
-  {
-    inputs: [],
-    name: "Withdraw",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    stateMutability: "payable",
-    type: "receive",
-  },
-  {
     inputs: [
-      {
-        internalType: "address",
-        name: "fundingAddress",
-        type: "address",
-      },
+      { internalType: "address", name: "fundingAddress", type: "address" },
     ],
     name: "getAddressToAmountFunded",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "index",
-        type: "uint256",
-      },
-    ],
+    inputs: [{ internalType: "uint256", name: "index", type: "uint256" }],
     name: "getFunder",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
+    outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
     name: "getOwner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
+    outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
     type: "function",
   },
-];
+  {
+    inputs: [],
+    name: "getVersion",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  { stateMutability: "payable", type: "receive" },
+]
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
-import {networkConfigs, abi, contractAddress } from "./constants.js"
+import { networkConfigs, abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
-const modal = document.getElementById('warningModal')
-const modalButton = document.getElementById('modalButton')
+const modal = document.getElementById("warningModal")
+const modalButton = document.getElementById("modalButton")
 const withdrawButton = document.getElementById("withdrawButton")
 const fundButton = document.getElementById("fundButton")
 const balanceSpan = document.querySelector(".balance")
@@ -21,120 +21,123 @@ let initialConnectAttempted = false
 
 // CONNECTION
 async function initiateConnectAttempt() {
-    if (!initialConnectAttempted) {
-        initialConnectAttempted = true
-        try {
-            await ethereum.request({ method: "eth_requestAccounts" })
-            await checkNetwork()
-            const accounts = await ethereum.request({ method: "eth_accounts" })
-            if (accounts.length > 0) {
-                const address = accounts[0]
-                displayTruncatedAddress(address)
-                displayENSName(address)
-            } else {
-                connectButton.innerHTML = "Connect"
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    } else {
-        connectButton.innerHTML = "Install a Wallet"
-        setTimeout(() => {
-            connectButton.innerHTML = "Connect"
-          }, 3000)
-    }
-}
-
-async function checkNetwork() {
-    const chainId = await ethereum.request({ method: 'eth_chainId' })
-    if (chainId !== `0x${currentNetwork.chainId.toString(16)}`) {
-        showModal()
-    } else {
-        modal.style.display = 'none'
-    }
-}
-
-function displayTruncatedAddress(address) {
-    if (!address) return
-    const truncatedAddress = `${address.substring(0, 6)}...${address.substring(address.length - 4)}`
-    connectButton.innerHTML = truncatedAddress
-}
-
-async function displayENSName(account) {
+  if (!initialConnectAttempted) {
+    initialConnectAttempted = true
     try {
-      const mainnetProvider = new ethers.providers.JsonRpcProvider(networkConfigs.ethereum.rpcUrl)
-      
-      const ensName = await mainnetProvider.lookupAddress(account)
-      
-      if (ensName) {
-        connectButton.innerHTML = ensName
-      }
-    } catch (error) {
-      console.log("Error getting ENS name:", error)
-    }
-  }
-
-function showModal() {
-    modal.style.display = 'block'
-}
-
-async function switchNetwork() {
-    try {
-        await ethereum.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: `0x${currentNetwork.chainId.toString(16)}` }]
-        })
-    } catch (error) {
-        console.error("Error switching network:", error)
-    }
-}
-
-// Event listener for changes in wallet accounts
-ethereum.on("accountsChanged", async function(accounts) {
-    if (accounts.length > 0) {
+      await ethereum.request({ method: "eth_requestAccounts" })
+      await checkNetwork()
+      const accounts = await ethereum.request({ method: "eth_accounts" })
+      if (accounts.length > 0) {
         const address = accounts[0]
         displayTruncatedAddress(address)
         displayENSName(address)
-    } else {
+      } else {
         connectButton.innerHTML = "Connect"
+      }
+    } catch (error) {
+      console.log(error)
     }
+  } else {
+    connectButton.innerHTML = "Install a Wallet"
+    setTimeout(() => {
+      connectButton.innerHTML = "Connect"
+    }, 3000)
+  }
+}
+
+async function checkNetwork() {
+  const chainId = await ethereum.request({ method: "eth_chainId" })
+  if (chainId !== `0x${currentNetwork.chainId.toString(16)}`) {
+    showModal()
+  } else {
+    modal.style.display = "none"
+  }
+}
+
+function displayTruncatedAddress(address) {
+  if (!address) return
+  const truncatedAddress = `${address.substring(0, 6)}...${address.substring(
+    address.length - 4
+  )}`
+  connectButton.innerHTML = truncatedAddress
+}
+
+async function displayENSName(account) {
+  try {
+    const mainnetProvider = new ethers.providers.JsonRpcProvider(
+      networkConfigs.ethereum.rpcUrl
+    )
+
+    const ensName = await mainnetProvider.lookupAddress(account)
+
+    if (ensName) {
+      connectButton.innerHTML = ensName
+    }
+  } catch (error) {
+    console.log("Error getting ENS name:", error)
+  }
+}
+
+function showModal() {
+  modal.style.display = "block"
+}
+
+async function switchNetwork() {
+  try {
+    await ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: `0x${currentNetwork.chainId.toString(16)}` }],
+    })
+  } catch (error) {
+    console.error("Error switching network:", error)
+  }
+}
+
+// Event listener for changes in wallet accounts
+ethereum.on("accountsChanged", async function (accounts) {
+  if (accounts.length > 0) {
+    const address = accounts[0]
+    displayTruncatedAddress(address)
+    displayENSName(address)
+  } else {
+    connectButton.innerHTML = "Connect"
+  }
 })
 // Event listener for changes in wallet network
-ethereum.on("chainChanged", async function(chainId) {
-    if (chainId !== `0x${currentNetwork.chainId.toString(16)}`) {
-        showModal()
-    } else {
-        modal.style.display = 'none'
-    }
+ethereum.on("chainChanged", async function (chainId) {
+  if (chainId !== `0x${currentNetwork.chainId.toString(16)}`) {
+    showModal()
+  } else {
+    modal.style.display = "none"
+  }
 })
 
 // Event listener to connect on page load if already connected
-window.addEventListener('load', async () => {
-    try {
-        const accounts = await ethereum.request({ method: "eth_accounts" })
-        if (accounts.length > 0) {
-            const address = accounts[0]
-            displayTruncatedAddress(address)
-            displayENSName(address)
-        }
-    } catch (error) {
-        console.log(error)
+window.addEventListener("load", async () => {
+  try {
+    const accounts = await ethereum.request({ method: "eth_accounts" })
+    if (accounts.length > 0) {
+      const address = accounts[0]
+      displayTruncatedAddress(address)
+      displayENSName(address)
     }
+  } catch (error) {
+    console.log(error)
+  }
 })
-
 
 // OTHER FUNCTIONS
 async function getBalance() {
-    if (typeof window.ethereum !== "undefined") {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      try {
-        const balance = await provider.getBalance(contractAddress)
-        balanceSpan.innerHTML = ethers.utils.formatEther(balance)
-      } catch (error) {
-        console.log(error)
-      }
-    } 
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    try {
+      const balance = await provider.getBalance(contractAddress)
+      balanceSpan.innerHTML = ethers.utils.formatEther(balance)
+    } catch (error) {
+      console.log(error)
+    }
   }
+}
 
 async function fund() {
   const ethAmount = document.getElementById("ethAmount").value
@@ -162,27 +165,26 @@ async function fund() {
 }
 
 async function withdraw() {
-    console.log(`Withdrawing...`)
-    if (typeof window.ethereum !== "undefined") {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      await provider.send('eth_requestAccounts', [])
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(contractAddress, abi, signer)
-      try {
-        const transactionResponse = await contract.Withdraw()
-        await listenForTransactionMine(transactionResponse, provider)
-        await getBalance()
-      } catch (error) {
-        console.log(error)
-      }
-    } else {
-      withdrawButton.innerHTML = "Install a Wallet"
-      setTimeout(() => {
-        withdrawButton.innerHTML = "Withdraw"
-      }, 2000)
+  console.log(`Withdrawing...`)
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    await provider.send("eth_requestAccounts", [])
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+      const transactionResponse = await contract.withdraw()
+      await listenForTransactionMine(transactionResponse, provider)
+      await getBalance()
+    } catch (error) {
+      console.log(error)
     }
+  } else {
+    withdrawButton.innerHTML = "Install a Wallet"
+    setTimeout(() => {
+      withdrawButton.innerHTML = "Withdraw"
+    }, 2000)
   }
-  
+}
 
 function listenForTransactionMine(transactionResponse, provider) {
   console.log(`Mining ${transactionResponse.hash}`)
@@ -196,4 +198,4 @@ function listenForTransactionMine(transactionResponse, provider) {
   })
 }
 
-getBalance();
+getBalance()
